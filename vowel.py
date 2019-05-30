14369c5b8525dcd0e40e228cc042e55849b35fb6
Vigneshwaran123 <51016246+Vigneshwaran123@users.noreply.github.com>
2019-05-30 06:53:43 +0530

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,7 @@
+ytt = input("")
+if (ytt=='a' or ytt=='e' or ytt=='i' or ytt=='o' or ytt=='u' or ytt=='A' or ytt=='E' or ytt=='I' or ytt=='O' or ytt=='U'):
+  print("Vowel")
+elif(ytt=='b' or ytt=='c' or ytt=='d' or ytt=='f' or ytt=='g' or ytt=='h' or ytt=='j' or ytt=='k' or ytt=='l' or ytt=='m' or ytt=='n' or ytt=='p' or ytt=='q' or ytt=='r' or ytt=='s' or ytt=='t' or ytt=='v' or ytt=='w' or ytt=='x' or ytt=='y' or ytt=='z' or ytt=='A' or ytt=='B' or ytt=='C' or ytt=='D' or ytt=='E' or ytt=='F' or ytt=='G' or ytt=='H' or ytt=='I' or ytt=='J' or ytt=='K' or ytt=='L' or ytt=='M' or ytt=='N' or ytt=='O' or ytt=='P' or ytt=='Q' or ytt=='R' or ytt=='S' or ytt=='T' or ytt=='U' or ytt=='V' or ytt=='W' or ytt=='X' or ytt=='Y' or ytt=='Z'):
+  print("Consonant")
+else:
+  print("invalid")
